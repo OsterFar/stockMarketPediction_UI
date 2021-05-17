e0ada052dac006b4a29cc9e8eae30cdd24d2c6b7
OsterFar <farazsarwar75@gmail.com>
2021-05-17 14:35:29 +0500

    ##

--- Stock_market_interface/crypto/views.py
+++ Stock_market_interface/crypto/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def cryptohomepage(request) :
-    return render(request , 'Cryto.html')
+    return render(request , 'CrytoMain.html')
